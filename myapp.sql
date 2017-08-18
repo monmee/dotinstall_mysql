4b37ca1ae27ed81809d5ba44f4f1bd96fe6a4a7b
monmee <jazzmonmee@gmail.com>
2017-08-18 09:57:53 +0900

    テーブルの構造を変えてみよう

--- myapp.sql
+++ myapp.sql
@@ -1,24 +1,16 @@
 drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
-  name varchar(20) unique,
-  -- score float not null
-  score float default 0.0
+  name varchar(20),
+  score float
 );
+
+-- alter table users add column email varchar(255);
+alter table users add column email varchar(255) after name;
+alter table users drop column score;
+alter table users change name user_name varchar(80) default 'nobody';
 desc users;
 
-insert into users (name, score) values ('taguchi', 8.3);
-insert into users (name, score) values ('fkoji', 5.6);
-insert into users (name, score) values ('dotinstall', 6.2);
-insert into users (name, score) values ('yamada', null);
-
-/*
-insert into users (id, name, score) values (1, 'taguchi', 8.3);
-insert into users (id, name, score) values (2, 'fkoji', 5.6);
-insert into users (id, name, score) values (3, 'dotinstall', 6.2);
-insert into users (id, name, score) values (4, 'yamada', null);
-insert into users (id, name, score) values (5, 'tanaka', 6.2);
-insert into users (id, name, score) values (6, 'tanaka', 6.2);
-*/
-
-select * from users;
+alter table users rename persons;
+show tables;
+drop table if exists persons;
