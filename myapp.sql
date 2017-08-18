8ba3d3d3e1ab00745ae0464aeb68faf768c108c0
monmee <jazzmonmee@gmail.com>
2017-08-18 09:41:01 +0900

    レコードを挿入してみよう2

--- myapp.sql
+++ myapp.sql
@@ -5,8 +5,17 @@ create table users (
   score float
 );
 
-insert into user (id, name, score) values (1, 'taguchi', 8.3)
-insert into user (id, name, score) values (2, 'fkoji', 5.6)
-insert into user (id, name, score) values (3, 'dotinstall', 6.2)
-insert into user (id, name, score) values (4, 'yamada', null)
+/*
+insert into users (id, name, score) values (1, 'taguchi', 8.3);
+insert into users (id, name, score) values (2, 'fkoji', 5.6);
+insert into users (id, name, score) values (3, 'dotinstall', 6.2);
+insert into users (id, name, score) values (4, 'yamada', null);
+*/
 
+insert into users (id, name, score) values
+  (1, 'taguchi', 8.3),
+  (2, 'fkoji', 5.6),
+  (3, 'dotinstall', 6.2),
+  (4, 'yamada', null);
+
+select * from users;
