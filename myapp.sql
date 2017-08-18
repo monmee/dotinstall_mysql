898eb41fa1759488f7ac3ed0a407c76f32e11d3c
monmee <jazzmonmee@gmail.com>
2017-08-18 09:34:39 +0900

    レコードを挿入してみよう

--- myapp.sql
+++ myapp.sql
@@ -1,29 +1,12 @@
-/*
-number:
-- int
-- float
-- double
-- int unsigned
-
-string:
-- char(4)
-- varchar(255)
-- text
-
-date/time:
-- date
-- time
-- datetime '2017-07-22 17:22:33'
-
-true/false:
-- boolean -> tinyint(1)
-  true -> 1
-  false -> 0
-*/
-
 drop table if exists users;
 create table users (
   id int unsigned,
   name varchar(20),
   score float
 );
+
+insert into user (id, name, score) values (1, 'taguchi', 8.3)
+insert into user (id, name, score) values (2, 'fkoji', 5.6)
+insert into user (id, name, score) values (3, 'dotinstall', 6.2)
+insert into user (id, name, score) values (4, 'yamada', null)
+
