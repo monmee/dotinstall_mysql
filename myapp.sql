fb5eb7712a80c48ff5220e0ff681c88e8c6b78e2
monmee <jazzmonmee@gmail.com>
2017-08-27 00:31:08 +0900

    外部キー制約を使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -8,7 +8,7 @@ create table posts (
 drop table if exists comments;
 create table comments (
   id int unsigned primary key auto_increment,
-  post_id int not null,
+  post_id int unsigned not null,
   body text
 );
 
@@ -16,13 +16,14 @@ insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
 insert into posts (title, body) values ('title 3', 'body 3');
 
+alter table comments add constraint fk_comments foreign key (post_id) references posts (id); 
+
 insert into comments (post_id, body) values (1, 'first comment for post 1');
 insert into comments (post_id, body) values (1, 'second comment for post 1');
 insert into comments (post_id, body) values (3, 'first comment for post 3');
 insert into comments (post_id, body) values (4, 'first comment for body 4');
 
--- select * from posts left outer join comments on posts.id = comments.post_id;
--- select * from posts right outer join comments on posts.id = comments.post_id;
+select * from posts;
+select * from comments;
 
-select * from posts left outer join comments on posts.id = comments.post_id;
-select * from posts left outer join comments on posts.id = comments.post_id;
+alter table comments drop foreign key fk_comments;
