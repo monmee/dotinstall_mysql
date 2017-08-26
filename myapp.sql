d77c56aa317bec7ef4bed8b73d3d0788749dc480
monmee <jazzmonmee@gmail.com>
2017-08-26 22:21:52 +0900

    viewで抽出条件を保存してみよう

--- myapp.sql
+++ myapp.sql
@@ -13,32 +13,9 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
-drop table if exists users_with_team;
-create table users_with_team as
-select
-  id,
-  name,
-  score,
-  case
-    when score > 8.0 then 'Team-A'
-    when score > 6.0 then 'Team-B'
-    else 'Team-C'
-  end as team
-from 
-  users;
+drop view if exists top3;
+create view top3 as select * from users order by score desc limit 3;
+select * from top3;
 
-select
-  sum(t.score), 
-  t.team 
-from
-  (select
-    id,
-    name,
-    score,
-    case
-      when score > 8.0 then 'Team-A'
-      when score > 6.0 then 'Team-B'
-      else 'Team-C'
-    end as team
-  from users) as t
-group by t.team;
+show tables;
+show create view top3;
