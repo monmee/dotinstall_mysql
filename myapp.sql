99f50446efb3b6298d5e59536f59b288def7c9df
monmee <jazzmonmee@gmail.com>
2017-08-26 23:57:19 +0900

    複数のテーブルを作ってみよう

--- myapp.sql
+++ myapp.sql
@@ -1,19 +1,25 @@
-drop table if exists users;
-create table users (
+drop table if exists posts;
+create table posts (
   id int unsigned primary key auto_increment,
-  name varchar(20),
-  score float
+  title varchar(255),
+  body text
 );
 
--- insert into users (name, score, coins) values ('taguchi', 5.8, 'gold,silver');
--- insert into users (name, score, coins) values ('fkoji', 8.2, 'bronze,gold');
--- insert into users (name, score, coins) values ('dotinstall', 6.1, 'bronze');
--- insert into users (name, score) values ('Tanaka', 4.2);
--- insert into users (name, score) values ('yamada', null);
--- insert into users (name, score) values ('tashiro', 7.9);
+drop table if exists comments;
+create table comments (
+  id int unsigned primary key auto_increment,
+  post_id int not null,
+  body text
+);
 
-alter table users add index index_score (score);
--- show index from users;
-explain select * from users where score > 5.0;
-explain select * from users where name = 'taguchi';
-alter table users drop index index_score;
+insert into posts (title, body) values ('title 1', 'body 1');
+insert into posts (title, body) values ('title 2', 'body 2');
+insert into posts (title, body) values ('title 3', 'body 3');
+
+insert into comments (post_id, body) values (1, 'first comment for post 1');
+insert into comments (post_id, body) values (1, 'second comment for post 2');
+insert into comments (post_id, body) values (3, 'first comment for post 3');
+insert into comments (post_id, body) values (4, 'first comment for body 4');
+
+select * from posts;
+select * from comments;
