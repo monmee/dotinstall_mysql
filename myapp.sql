7dd89f6c6c165b8cda77b15ec3def7af8537e679
monmee <jazzmonmee@gmail.com>
2017-08-26 16:34:02 +0900

    if､caseを使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -13,7 +13,19 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
-select * from users;
-select * from users where coins = 'gold,silver';
-select * from users where coins like '%gold%';
-select * from users where coins = 3;
+select
+  name,
+  score,
+  case floor(score) % 2
+    when 0 then 'even'
+    when 1 then 'odd'
+  end as type
+from 
+  users;
+
+select
+  name,
+  score,
+  if (score > 5.0, 'OK', 'NG') as result
+from 
+  users;
