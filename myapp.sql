8ff89c739f47d45d9ba246c006f1aa94c63eb363
monmee <jazzmonmee@gmail.com>
2017-08-26 23:24:33 +0900

    索引を設定してみよう

--- myapp.sql
+++ myapp.sql
@@ -2,21 +2,18 @@ drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
-  score float,
-  coins set('gold', 'silver', 'bronze')
+  score float
 );
 
-insert into users (name, score, coins) values ('taguchi', 5.8, 'gold,silver');
-insert into users (name, score, coins) values ('fkoji', 8.2, 'bronze,gold');
-insert into users (name, score, coins) values ('dotinstall', 6.1, 'bronze');
-insert into users (name, score) values ('Tanaka', 4.2);
-insert into users (name, score) values ('yamada', null);
-insert into users (name, score) values ('tashiro', 7.9);
+-- insert into users (name, score, coins) values ('taguchi', 5.8, 'gold,silver');
+-- insert into users (name, score, coins) values ('fkoji', 8.2, 'bronze,gold');
+-- insert into users (name, score, coins) values ('dotinstall', 6.1, 'bronze');
+-- insert into users (name, score) values ('Tanaka', 4.2);
+-- insert into users (name, score) values ('yamada', null);
+-- insert into users (name, score) values ('tashiro', 7.9);
 
-start transaction;
-update users set score = score - 1.2 where name = 'fkoji';
-update users set score = score + 1.2 where name = 'taguchi';
--- commit;
-rollback;
-
-select * from users;
+alter table users add index index_score (score);
+-- show index from users;
+explain select * from users where score > 5.0;
+explain select * from users where name = 'taguchi';
+alter table users drop index index_score;
