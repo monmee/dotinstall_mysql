02ef4cbb7fce8b5cb5e82ad309cf729f77ce1acb
monmee <jazzmonmee@gmail.com>
2017-08-26 16:28:19 +0900

    set型を使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -3,15 +3,17 @@ create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
   score float,
-  rank enum('gold', 'silver', 'blonze')
+  coins set('gold', 'silver', 'bronze')
 );
 
-insert into users (name, score, rank) values ('taguchi', 5.8, 'silver');
-insert into users (name, score, rank) values ('fkoji', 8.2, 'gold');
-insert into users (name, score, rank) values ('dotinstall', 6.1, 'blonze');
+insert into users (name, score, coins) values ('taguchi', 5.8, 'gold,silver');
+insert into users (name, score, coins) values ('fkoji', 8.2, 'bronze,gold');
+insert into users (name, score, coins) values ('dotinstall', 6.1, 'bronze');
 insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
-select * from users where rank = 'silver';
-select * from users where rank = 2;
+select * from users;
+select * from users where coins = 'gold,silver';
+select * from users where coins like '%gold%';
+select * from users where coins = 3;
