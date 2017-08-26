0908d48e0c3f5a00d367db8faf8279aaf17b50fb
monmee <jazzmonmee@gmail.com>
2017-08-26 16:22:04 +0900

    enum型を使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -2,22 +2,16 @@ drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
-  score float
+  score float,
+  rank enum('gold', 'silver', 'blonze')
 );
 
-insert into users (name, score) values ('taguchi', 5.8);
-insert into users (name, score) values ('fkoji', 8.2);
-insert into users (name, score) values ('dotinstall', 6.1);
+insert into users (name, score, rank) values ('taguchi', 5.8, 'silver');
+insert into users (name, score, rank) values ('fkoji', 8.2, 'gold');
+insert into users (name, score, rank) values ('dotinstall', 6.1, 'blonze');
 insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
-select length('Hello');
-select substr('Hello', 2);
-select substr('Hello', 2, 3);
-select upper('Hello');
-select lower('Hello');
-select concat('Hello', 'world');
-
-select length(name) as len, name from users order by len;
-
+select * from users where rank = 'silver';
+select * from users where rank = 2;
