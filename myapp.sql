749a2f873c8be693383b4f2f27862cd6c49bf0a4
monmee <jazzmonmee@gmail.com>
2017-08-26 22:14:38 +0900

    サブクエリを使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -27,7 +27,18 @@ select
 from 
   users;
 
-select sum(score), team from users_with_team group by team;
-select sum(score), team from users_with_team group by team desc;
-select sum(score), team from users_with_team group by team having sum(score) > 10.0;
-select sum(score), team from users_with_team where id > 3 group by team;
+select
+  sum(t.score), 
+  t.team 
+from
+  (select
+    id,
+    name,
+    score,
+    case
+      when score > 8.0 then 'Team-A'
+      when score > 6.0 then 'Team-B'
+      else 'Team-C'
+    end as team
+  from users) as t
+group by t.team;
